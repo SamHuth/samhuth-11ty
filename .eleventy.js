c15d8bf13e15b83063549e1557e1f28f388c871a
SamHuth <sams.huth@gmail.com>
2025-12-25 05:45:26 +1000

    dynamic date

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy({ 'robots.txt': 'robots.txt' });
 
+  // Current Year
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
   // Add target=blank custom code
   const mdOptions = {
     html: true,
